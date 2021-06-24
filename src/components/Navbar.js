36b5cc1c60e05d406e558a6ca0bffd3bb271c4cc
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-06-24 18:08:19 -0300

    Responsive Navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import { FaBars, FaTimes  } from 'react-icons/fa'
 
 import logo from '../assets/images/logo-h.svg'
 
 import '../styles/navbar.scss'
 
 export function Navbar() {
+  const [click, setClick] = useState(false)
+
+  function handleClick() {
+    setClick(!click)
+  }
+
   return(
     <nav id='navbar'>
       <div className="nav-container">
         <img src={logo} alt="IMC" />
+        <div className="hamburger-menu" onClick={handleClick}>
+            { click ? (<FaTimes />) : (<FaBars />)}
+        </div>
         <div className="nav-menu-wrapper">
-          <ul className="nav-menu">
+          <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
             <li className="nav-item">
               <Link path='/calculate' className="nav-link">Calcular</Link>
             </li>
