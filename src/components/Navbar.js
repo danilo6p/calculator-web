df1de53e2a496baea0939736a94fbfb3439ee7ef
Matheus Germano da Costa <62772781+matheus-germano@users.noreply.github.com>
2021-06-24 17:43:12 -0300

    Navbar done without responsivity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,27 @@
+import { Link } from 'react-router-dom'
+
 import logo from '../assets/images/logo-h.svg'
+
+import '../styles/navbar.scss'
 
 export function Navbar() {
   return(
-    <nav>
-      <div id="navbar-container">
+    <nav id='navbar'>
+      <div className="nav-container">
         <img src={logo} alt="IMC" />
+        <div className="nav-menu-wrapper">
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <Link path='/calculate' className="nav-link">Calcular</Link>
+            </li>
+            <li className="nav-item">
+              <Link path='/results' className="nav-link">Resultados</Link>
+            </li>
+            <li className="nav-item">
+              <Link path='/about' className="nav-link">Saiba mais</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
